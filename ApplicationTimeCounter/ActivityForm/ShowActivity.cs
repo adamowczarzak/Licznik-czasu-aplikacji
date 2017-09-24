d968962402fe06a769b4d51e162fd8c51527dfca
owca25 <owca250@gmail.com>
2017-09-24 20:36:38 +0200

    ShowActivity - Create interface.
    ShowActivity - Create interface.

--- ApplicationTimeCounter/ActivityForm/ShowActivity.cs
+++ ApplicationTimeCounter/ActivityForm/ShowActivity.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace ApplicationTimeCounter
@@ -12,11 +15,143 @@ namespace ApplicationTimeCounter
     {
         private Canvas canvas;
         private Canvas mainCanvas;
+        private Canvas contentCanvas;
+        private MyLabel nameActivity;
 
         public ShowActivity(ref Canvas canvas)
         {
             this.canvas = canvas;
             mainCanvas = CanvasCreator.CreateCanvas(canvas, 620, 410, Color.FromArgb(255, 226, 240, 255), 0, 0);
+            this.canvas.KeyDown += mainCanvas_KeyDown;
+            contentCanvas = CanvasCreator.CreateCanvas(mainCanvas, 620, 320, Color.FromArgb(255, 236, 236, 236), 0, 50);
+
+            new MyRectangle(mainCanvas, 620, 1, Color.FromArgb(30, 110, 110, 110), 0, 50);
+            new MyRectangle(mainCanvas, 620, 1, Color.FromArgb(50, 110, 110, 110), 0, 370);
+
+            nameActivity = new MyLabel(mainCanvas, "Nazwa aktywnosci", 300, 36, 16, 15, 15, Color.FromArgb(255, 160, 160, 160),
+                Color.FromArgb(0, 100, 100, 100), horizontalAlignment: HorizontalAlignment.Left);
+            nameActivity.SetFont("Verdana");
+
+            CreateControlUser();
+            CreateChart();
+            this.canvas.Focusable = true;
+            this.canvas.Focus();
+            
+        }
+
+        private void mainCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Right)
+            {                
+                // zmiana danych ++
+            }
+            if (e.Key == Key.Left)
+            {
+                // zmiana danych --
+            }
+            if (e.Key == Key.Left || e.Key == Key.Right)
+            {
+                BackgroundWorker backgroundWorkerLoadingWindow = new BackgroundWorker();
+                backgroundWorkerLoadingWindow.DoWork += backgroundWorkerLoadingWindow_DoWork;
+                backgroundWorkerLoadingWindow.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorkerLoadingWindow_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Canvas canvasLoad = null;
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                canvasLoad = CanvasCreator.CreateCanvas(mainCanvas, (int)mainCanvas.Width, (int)mainCanvas.Height, Color.FromArgb(255, 255, 255, 255), 0, 0);
+                canvasLoad.Opacity = 0;
+            });
+
+            for (int i = 0; i < 30; i++)
+            {
+                Application.Current.Dispatcher.Invoke(() => { canvasLoad.Opacity += 0.04; });
+                Thread.Sleep(20);
+            }
+            for (int i = 0; i < 30; i++)
+            {
+                Application.Current.Dispatcher.Invoke(() => { canvasLoad.Opacity -= 0.04; });
+                Thread.Sleep(20);
+            }
+            Application.Current.Dispatcher.Invoke(() => { mainCanvas.Children.Remove(canvasLoad); this.canvas.Focus(); });
+        }
+
+        private void CreateControlUser()
+        {
+            new MyLabel(mainCanvas, "+", 30, 50, 30, 530, 6, Color.FromArgb(255, 70, 70, 70),
+                Color.FromArgb(255, 70, 70, 70), 0);
+
+            Label buttonAdd = ButtonCreator.CreateButton(mainCanvas, "", 30, 30, 14, 530, 20,
+                Color.FromArgb(0, 155, 155, 155), Color.FromArgb(0, 155, 155, 155), 0);
+            buttonAdd.Background = new SolidColorBrush(Color.FromArgb(180, 215, 215, 215));
+            buttonAdd.MouseMove += buttonAdd_MouseMove;
+            buttonAdd.MouseLeave += buttonAdd_MouseLeave;
+
+            new MyLabel(mainCanvas, "-", 30, 50, 30, 560, 6, Color.FromArgb(255, 70, 70, 70),
+               Color.FromArgb(255, 70, 70, 70), 0);
+
+            Label buttonDelete = ButtonCreator.CreateButton(mainCanvas, "", 30, 30, 14, 560, 20,
+                Color.FromArgb(0, 155, 155, 155), Color.FromArgb(0, 155, 155, 155), 0);
+            buttonDelete.Background = new SolidColorBrush(Color.FromArgb(180, 215, 215, 215));
+            buttonDelete.MouseMove += buttonDelete_MouseMove;
+            buttonDelete.MouseLeave += buttonDelete_MouseLeave;
+
+            new MyLabel(mainCanvas, "x", 30, 50, 24, 590, 10, Color.FromArgb(255, 70, 70, 70),
+                Color.FromArgb(255, 70, 70, 70), 0);
+
+            Label buttonExit = ButtonCreator.CreateButton(mainCanvas, "", 30, 30, 14, 590, 20,
+                Color.FromArgb(0, 155, 155, 155), Color.FromArgb(0, 155, 155, 155), 0);
+            buttonExit.Background = new SolidColorBrush(Color.FromArgb(180, 215, 215, 215));
+            buttonExit.MouseMove += buttonExit_MouseMove;
+            buttonExit.MouseLeave += buttonExit_MouseLeave;
+            buttonExit.MouseLeftButtonDown += buttonExit_MouseLeftButtonDown;
+        }
+
+        private void CreateChart()
+        {
+            for(int i = 0; i < 7; i++)
+            {
+                MyRectangle colorLegend = new MyRectangle(contentCanvas, 16, 120, Color.FromArgb(150, 150, 150, 150), 100 + (i * 40), 80);
+            }
+        }
+
+        private void buttonExit_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            mainCanvas.Children.RemoveRange(0, mainCanvas.Children.Count);
+            this.canvas.Children.Remove(mainCanvas);
+        }
+
+        private void buttonExit_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(180, 215, 215, 215));
+        }
+
+        private void buttonExit_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(120, 132, 177, 255));
+        }
+
+        private void buttonDelete_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(180, 215, 215, 215));
+        }
+
+        private void buttonDelete_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(120, 255, 132, 132));
+        }
+
+        private void buttonAdd_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(180, 215, 215, 215));
+        }
+
+        private void buttonAdd_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            (sender as Label).Background = new SolidColorBrush(Color.FromArgb(120, 132, 255, 140));
         }
     }
 }
